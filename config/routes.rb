628642b93fb7b8cc7a3b3f0760c60ae3fa8e64e1
Arthur Littmann <arthur.littmann@gmail.com>
2017-01-06 12:25:04 +0000

    contact page improved

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+
   resources :peintures, only: [:new, :create, :show, :edit, :update, :destroy]
 
   root to: 'pages#home'
+  get '/contact', to: 'pages#contact'
+
 
   mount Attachinary::Engine => "/attachinary"
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
